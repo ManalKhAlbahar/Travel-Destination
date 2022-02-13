7f9721b6b2c6a8c283c542f3cc0510a1c24fb636
Manal Albahar <manal.albahar@gmail.com>
2022-02-14 00:05:07 +0200

    react app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <>
+            <div id="fid">
+                <a href="https://www.facebook.com">Facebook</a><br/>
+                <a href="https://www.instagram.com">Instagram</a>
+                <p>Author: Manal Albahar</p>
+            </div>
+        </>
+    )
+}
